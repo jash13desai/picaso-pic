15c33a8226a401485a670b4df17afbce977a861b
jade13 <snerz13@gmail.com>
2021-07-18 23:35:27 +0530

    .

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -15,176 +15,182 @@ import 'package:insta_ui_only/models/user.dart';
 import 'package:insta_ui_only/myColors.dart';
 import 'package:insta_ui_only/myFonts.dart';
 import 'package:insta_ui_only/mySpaces.dart';
-import 'package:insta_ui_only/providers/DUMMY_DATA.dart';
+import 'package:insta_ui_only/providers/misc_data.dart';
 import 'package:insta_ui_only/sizeConfig.dart';
 
 class AccountPage extends StatelessWidget {
   final User data = DummyData().currentUser;
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        color: MediaQuery.of(context).platformBrightness == Brightness.light
-            ? kBlack
-            : kWhite,
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      StoryWidget(
-                        storyImage: AssetImage("assets/images/labyrinth.jpg"),
-                        storyName: 'Your Story',
-                      ),
-                      // Container(
-                      //   padding: const EdgeInsets.all(5),
-                      //   decoration: BoxDecoration(
-                      //       shape: BoxShape.circle,
-                      //       border: Border.all(
-                      //         width: 1,
-                      //         color: storyColor,
-                      //       )),
-                      //   child: CircleAvatar(
-                      //     backgroundImage: AssetImage(data.imageUrl),
-                      //     radius: SizeConfig.horizontalBlockSize * 12,
-                      //   ),
-                      // ),
-                      MySpaces.hGapInBetween,
-                      Expanded(
-                        child: DefaultTextStyle(
-                          style: MyFonts.medium.size(18),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Column(
-                                children: [
-                                  Text("${data.postNum}"),
-                                  Text("Posts"),
-                                ],
-                              ),
-                              Column(
-                                children: [
-                                  Text("${data.followers}"),
-                                  Text("Followers"),
-                                ],
-                              ),
-                              Column(
-                                children: [
-                                  Text("${data.following}"),
-                                  Text("Following"),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  MySpaces.vGapInBetween,
-                  Text(
-                    data.displayname,
-                    style: MyFonts.light.size(15),
-                  ),
-                  Text(
-                    data.bio,
-                    style: MyFonts.light.size(15),
-                  ),
-                  MySpaces.vGapInBetween,
-                  Container(
-                    width: double.infinity,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Edit Profile",
-                        style: MyFonts.light.setColor(kWhite).size(17),
-                      ),
-                      style: TextButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(vertical: 7),
-                        backgroundColor: kBlack,
-                        shape: RoundedRectangleBorder(
-                          side: BorderSide(
-                            color: MediaQuery.of(context).platformBrightness ==
-                                    Brightness.dark
-                                ? kWhite.withOpacity(0.5)
-                                : kBlack.withOpacity(0.5),
-                            width: 0.5,
-                          ),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(8),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  MySpaces.vSmallestGapInBetween,
-                  Container(
-                    // color: Colors.green,
-                    padding: EdgeInsets.symmetric(vertical: 10),
-                    height: SizeConfig.verticalBlockSize * 14,
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          color: MediaQuery.of(context).platformBrightness == Brightness.light
+              ? kWhite
+              : kBlack,
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 50, horizontal: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Container(
-                          padding: const EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              border: Border.all(
-                                width: 1,
-                                color: storyColor,
-                              )),
-                          child: CircleAvatar(
-                            child: Icon(
-                              Icons.add,
-                              size: SizeConfig.horizontalBlockSize * 8,
+                        StoryWidget(
+                          storyImage: AssetImage("assets/images/labyrinth.jpg"),
+                          storyName: '',
+                        ),
+                        // Container(
+                        //   padding: const EdgeInsets.all(5),
+                        //   decoration: BoxDecoration(
+                        //       shape: BoxShape.circle,
+                        //       border: Border.all(
+                        //         width: 1,
+                        //         color: storyColor,
+                        //       )),
+                        //   child: CircleAvatar(
+                        //     backgroundImage: AssetImage(data.imageUrl),
+                        //     radius: SizeConfig.horizontalBlockSize * 12,
+                        //   ),
+                        // ),
+                        MySpaces.hGapInBetween,
+                        Expanded(
+                          child: DefaultTextStyle(
+                            style: MyFonts.medium.size(18),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Column(
+                                  children: [
+                                    Text("${data.postNum}"),
+                                    Text("Posts"),
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text("${data.followers}"),
+                                    Text("Followers"),
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text("${data.following}"),
+                                    Text("Following"),
+                                  ],
+                                ),
+                              ],
                             ),
-                            radius: SizeConfig.horizontalBlockSize * 8,
                           ),
-                        ),
-                        MySpaces.hGapInBetween,
-                        StoryWidget(
-                          storyImage: AssetImage("assets/images/labyrinth.jpg"),
-                          storyName: 'Your Story',
-                        ),
-                        MySpaces.hGapInBetween,
-                        StoryWidget(
-                          storyImage: AssetImage("assets/images/labyrinth.jpg"),
-                          storyName: 'Your Story',
-                        ),
-                        MySpaces.hGapInBetween,
-                        StoryWidget(
-                          storyImage: AssetImage("assets/images/labyrinth.jpg"),
-                          storyName: 'Your Story',
                         ),
                       ],
                     ),
-                  ),
-                ],
+                    MySpaces.vGapInBetween,
+                    Text(
+                      data.displayname,
+                      style: MyFonts.light.size(15),
+                    ),
+                    Text(
+                      data.bio,
+                      style: MyFonts.light.size(15),
+                    ),
+                    MySpaces.vGapInBetween,
+                    Container(
+                      width: double.infinity,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Edit Profile",
+                          style: MyFonts.light.setColor(kWhite).size(17),
+                        ),
+                        style: TextButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(vertical: 7),
+                          backgroundColor: kBlack,
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                              color:
+                                  MediaQuery.of(context).platformBrightness ==
+                                          Brightness.dark
+                                      ? kWhite.withOpacity(0.5)
+                                      : kBlack.withOpacity(0.5),
+                              width: 0.5,
+                            ),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    MySpaces.vSmallestGapInBetween,
+                    Container(
+                      // color: Colors.green,
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      height: SizeConfig.verticalBlockSize * 14,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  width: 1,
+                                  color: storyColor,
+                                )),
+                            child: CircleAvatar(
+                              child: Icon(
+                                Icons.add,
+                                size: SizeConfig.horizontalBlockSize * 8,
+                              ),
+                              radius: SizeConfig.horizontalBlockSize * 8,
+                            ),
+                          ),
+                          MySpaces.hGapInBetween,
+                          StoryWidget(
+                            storyImage:
+                                AssetImage("assets/images/labyrinth.jpg"),
+                            storyName: 'Your Story',
+                          ),
+                          MySpaces.hGapInBetween,
+                          StoryWidget(
+                            storyImage:
+                                AssetImage("assets/images/labyrinth.jpg"),
+                            storyName: 'Your Story',
+                          ),
+                          MySpaces.hGapInBetween,
+                          StoryWidget(
+                            storyImage:
+                                AssetImage("assets/images/labyrinth.jpg"),
+                            storyName: 'Your Story',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Divider(
-              color: kWhite.withOpacity(0.2),
-              height: 0.5,
-            ),
-            MySpaces.vGapInBetween,
-            GridView.builder(
-              physics: NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: 5,
-                mainAxisSpacing: 5,
+              Divider(
+                color: kWhite.withOpacity(0.2),
+                height: 0.5,
               ),
-              itemCount: data.userPosts.length,
-              itemBuilder: (ctx, index) {
-                return Image.asset(data.userPosts[index]);
-              },
-            )
-          ],
+              MySpaces.vGapInBetween,
+              GridView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 5,
+                  mainAxisSpacing: 5,
+                ),
+                itemCount: data.userPosts.length,
+                itemBuilder: (ctx, index) {
+                  return Image.asset(data.userPosts[index]);
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -4,77 +4,89 @@ import 'package:insta_ui_only/myColors.dart';
 import 'package:insta_ui_only/myFonts.dart';
 import 'package:insta_ui_only/mySpaces.dart';
 import 'package:insta_ui_only/sizeConfig.dart';
-import 'package:insta_ui_only/providers/DUMMY_DATA.dart';
+import 'package:insta_ui_only/providers/misc_data.dart';
 import 'package:insta_ui_only/widgets/searchbar_widget.dart';
 
 class SearchPage extends StatelessWidget {
   final data = DummyData();
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-          color: MediaQuery.of(context).platformBrightness == Brightness.light
-              ? kBlack
-              : kWhite,
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.fromLTRB(
-                  0,
-                  50,
-                  0,
-                  0,
+    return Material(
+      child: SingleChildScrollView(
+        child: Container(
+            color: MediaQuery.of(context).platformBrightness == Brightness.light
+                ? kBlack
+                : kWhite,
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(
+                    0,
+                    50,
+                    0,
+                    0,
+                  ),
+                  child: SearchBar(),
                 ),
-                child: SearchBar(),
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    ...["IGTV", "Shop", "Style", "Sports", "Auto", "Random"]
-                        .map((text) {
-                      return Container(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 9, vertical: 3),
-                        child: TextButton(
-                            style: TextButton.styleFrom(
-                              padding: EdgeInsets.symmetric(vertical: 8),
-                              backgroundColor: kBlack,
-                              shape: RoundedRectangleBorder(
-                                side: BorderSide(
-                                    color: kGrey.withOpacity(0.5), width: 1),
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(8),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      ...["IGTV", "Shop", "Style", "Sports", "Auto", "Random"]
+                          .map((text) {
+                        return Container(
+                          margin:
+                              EdgeInsets.symmetric(horizontal: 9, vertical: 3),
+                          child: TextButton(
+                              style: TextButton.styleFrom(
+                                padding: EdgeInsets.symmetric(vertical: 8),
+                                backgroundColor:
+                                    MediaQuery.of(context).platformBrightness ==
+                                            Brightness.light
+                                        ? kWhite
+                                        : kBlack,
+                                shape: RoundedRectangleBorder(
+                                  side: BorderSide(
+                                      color: kGrey.withOpacity(0.5), width: 1),
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(8),
+                                  ),
                                 ),
                               ),
-                            ),
-                            onPressed: () {},
-                            child: Text(
-                              text,
-                              style: MyFonts.medium
-                                  .setColor(kWhite)
-                                  .size(SizeConfig.horizontalBlockSize * 4),
-                            )),
-                      );
-                    }).toList()
-                  ],
+                              onPressed: () {},
+                              child: Text(
+                                text,
+                                style: MyFonts.medium
+                                    .setColor(
+                                      MediaQuery.of(context)
+                                                  .platformBrightness ==
+                                              Brightness.light
+                                          ? kBlack
+                                          : kWhite,
+                                    )
+                                    .size(SizeConfig.horizontalBlockSize * 4),
+                              )),
+                        );
+                      }).toList()
+                    ],
+                  ),
                 ),
-              ),
-              MySpaces.vGapInBetween,
-              StaggeredGridView.countBuilder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                crossAxisCount: 3,
-                itemCount: data.searchImages.length,
-                itemBuilder: (context, index) =>
-                    Image.asset(data.searchImages[index]),
-                staggeredTileBuilder: (index) => StaggeredTile.count(
-                    (index % 10 == 0) ? 2 : 1, (index % 10 == 0) ? 2 : 1),
-                mainAxisSpacing: 8.0,
-                crossAxisSpacing: 8.0,
-              )
-            ],
-          )),
+                MySpaces.vGapInBetween,
+                StaggeredGridView.countBuilder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  crossAxisCount: 3,
+                  itemCount: data.searchImages.length,
+                  itemBuilder: (context, index) =>
+                      Image.asset(data.searchImages[index]),
+                  staggeredTileBuilder: (index) => StaggeredTile.count(
+                      (index % 10 == 0) ? 2 : 1, (index % 10 == 0) ? 2 : 1),
+                  mainAxisSpacing: 8.0,
+                  crossAxisSpacing: 8.0,
+                )
+              ],
+            )),
+      ),
     );
   }
 }

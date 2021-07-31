6140df64831d928cbe4cb642f817e7a0b47b97a5
= <=>
2021-07-31 19:54:18 +0530

    refresh by swipe

--- lib/screens/homeBar_screen.dart
+++ lib/screens/homeBar_screen.dart
@@ -35,131 +35,143 @@ class _InstaHomeState extends State<InstaHome> {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     imageUpload.init(context);
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () => imageUpload.navigate(),
-          icon: Icon(
-            Icons.camera_alt,
-            color: MediaQuery.of(context).platformBrightness == Brightness.light
-                ? kBlack
-                : kWhite,
-          ),
-        ),
-        title: SizedBox(
-          height: 35.0,
-          child: MediaQuery.of(context).platformBrightness == Brightness.light
-              ? Image.asset("assets/images/insta_logo_light.jpg")
-              : Image.asset("assets/images/insta_logo_dark.jpg"),
-        ),
-        actions: <Widget>[
-          IconButton(
-            onPressed: () => {},
-            icon: MediaQuery.of(context).platformBrightness == Brightness.light
-                ? Image.asset('assets/icons/igtv_icon_light.jpg')
-                : Image.asset('assets/icons/igtv_icon_dark.jpg'),
-            iconSize: 5,
-          ),
-          Padding(
-            padding: EdgeInsets.only(right: 12.0),
-            child: IconButton(
-              icon: Icon(FontAwesomeIcons.paperPlane),
-              iconSize: 25,
-              onPressed: () {
-                Navigator.of(context).pushReplacementNamed(DMPage.route);
-              },
+    return RefreshIndicator(
+      onRefresh: () async {
+        await Provider.of<Posts>(context, listen: false).fetchAndSetPosts();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () => imageUpload.navigate(),
+            icon: Icon(
+              Icons.camera_alt,
+              color:
+                  MediaQuery.of(context).platformBrightness == Brightness.light
+                      ? kBlack
+                      : kWhite,
             ),
           ),
-        ],
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Flexible(
-            child: InstaList(),
+          title: SizedBox(
+            height: 35.0,
+            child: MediaQuery.of(context).platformBrightness == Brightness.light
+                ? Image.asset("assets/images/insta_logo_light.jpg")
+                : Image.asset("assets/images/insta_logo_dark.jpg"),
           ),
-        ],
-      ),
-      bottomNavigationBar: Container(
-        color: MediaQuery.of(context).platformBrightness == Brightness.light
-            ? Colors.white
-            : Colors.black,
-        height: 50.0,
-        alignment: Alignment.center,
-        child: BottomAppBar(
+          actions: <Widget>[
+            IconButton(
+              onPressed: () => {},
+              icon:
+                  MediaQuery.of(context).platformBrightness == Brightness.light
+                      ? Image.asset('assets/icons/igtv_icon_light.jpg')
+                      : Image.asset('assets/icons/igtv_icon_dark.jpg'),
+              iconSize: 5,
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 12.0),
+              child: IconButton(
+                icon: Icon(FontAwesomeIcons.paperPlane),
+                iconSize: 25,
+                onPressed: () {
+                  Navigator.of(context).pushReplacementNamed(DMPage.route);
+                },
+              ),
+            ),
+          ],
+        ),
+        body: RefreshIndicator(
+          onRefresh: () async {
+            await Provider.of<Posts>(context, listen: false).fetchAndSetPosts();
+          },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              InstaList(),
+            ],
+          ),
+        ),
+        bottomNavigationBar: Container(
           color: MediaQuery.of(context).platformBrightness == Brightness.light
               ? Colors.white
               : Colors.black,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              IconButton(
-                color: MediaQuery.of(context).platformBrightness ==
-                        Brightness.light
-                    ? Colors.black
-                    : Colors.white,
-                icon: Icon(
-                  Icons.home,
+          height: 50.0,
+          alignment: Alignment.center,
+          child: BottomAppBar(
+            color: MediaQuery.of(context).platformBrightness == Brightness.light
+                ? Colors.white
+                : Colors.black,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                IconButton(
+                  color: MediaQuery.of(context).platformBrightness ==
+                          Brightness.light
+                      ? Colors.black
+                      : Colors.white,
+                  icon: Icon(
+                    Icons.home,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pushReplacementNamed(InstaHome.route);
+                  },
                 ),
-                onPressed: () {
-                  Navigator.of(context).pushReplacementNamed(InstaHome.route);
-                },
-              ),
-              IconButton(
-                color: MediaQuery.of(context).platformBrightness ==
-                        Brightness.light
-                    ? Colors.grey.shade700
-                    : Colors.grey.shade700,
-                icon: Icon(
-                  Icons.search,
+                IconButton(
+                  color: MediaQuery.of(context).platformBrightness ==
+                          Brightness.light
+                      ? Colors.grey.shade700
+                      : Colors.grey.shade700,
+                  icon: Icon(
+                    Icons.search,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context)
+                        .pushReplacementNamed(SearchPage.route);
+                  },
                 ),
-                onPressed: () {
-                  Navigator.of(context).pushReplacementNamed(SearchPage.route);
-                },
-              ),
-              IconButton(
-                color: MediaQuery.of(context).platformBrightness ==
-                        Brightness.light
-                    ? Colors.black
-                    : Colors.white,
-                icon: Icon(
-                  Icons.add_box,
-                ),
-                // onPressed: () => imageUpload.pickImage(ImageSource.gallery),
-                onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => AddPost(ImageSource.gallery),
+                IconButton(
+                  color: MediaQuery.of(context).platformBrightness ==
+                          Brightness.light
+                      ? Colors.black
+                      : Colors.white,
+                  icon: Icon(
+                    Icons.add_box,
+                  ),
+                  // onPressed: () => imageUpload.pickImage(ImageSource.gallery),
+                  onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AddPost(ImageSource.gallery),
+                    ),
                   ),
                 ),
-              ),
-              IconButton(
-                color: MediaQuery.of(context).platformBrightness ==
-                        Brightness.light
-                    ? Colors.grey.shade700
-                    : Colors.grey.shade700,
-                icon: Icon(FontAwesomeIcons.heart),
-                iconSize: 20,
-                onPressed: () {
-                  Navigator.of(context)
-                      .pushReplacementNamed(ActivityPage.route);
-                },
-              ),
-              IconButton(
-                color: MediaQuery.of(context).platformBrightness ==
-                        Brightness.light
-                    ? Colors.grey.shade700
-                    : Colors.grey.shade700,
-                icon: Icon(
-                  MediaQuery.of(context).platformBrightness == Brightness.dark
-                      ? Icons.account_box
-                      : Icons.account_box_outlined,
+                IconButton(
+                  color: MediaQuery.of(context).platformBrightness ==
+                          Brightness.light
+                      ? Colors.grey.shade700
+                      : Colors.grey.shade700,
+                  icon: Icon(FontAwesomeIcons.heart),
+                  iconSize: 20,
+                  onPressed: () {
+                    Navigator.of(context)
+                        .pushReplacementNamed(ActivityPage.route);
+                  },
                 ),
-                onPressed: () {
-                  Navigator.of(context).pushReplacementNamed(AccountPage.route);
-                },
-              ),
-            ],
+                IconButton(
+                  color: MediaQuery.of(context).platformBrightness ==
+                          Brightness.light
+                      ? Colors.grey.shade700
+                      : Colors.grey.shade700,
+                  icon: Icon(
+                    MediaQuery.of(context).platformBrightness == Brightness.dark
+                        ? Icons.account_box
+                        : Icons.account_box_outlined,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context)
+                        .pushReplacementNamed(AccountPage.route);
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),

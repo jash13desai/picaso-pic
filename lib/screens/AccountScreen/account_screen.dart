42d14a1e5160111019e51007cdc406617b9807d1
= <jash.13.desai@gmail.com>
2021-08-13 08:21:52 +0530

    Optimized!

--- lib/screens/AccountScreen/account_screen.dart
+++ lib/screens/AccountScreen/account_screen.dart
@@ -1,19 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_auth/firebase_auth.dart' as db;
 import 'package:flutter/material.dart';
 
 import 'package:insta_ui_only/globals/myFonts.dart';
 import 'package:insta_ui_only/globals/mySpaces.dart';
 import 'package:insta_ui_only/globals/sizeConfig.dart';
-import 'package:insta_ui_only/providers/misc_data.dart';
+
 import 'package:insta_ui_only/screens/IntroScreen/login_screen.dart';
-import 'package:insta_ui_only/screens/IntroScreen/signup_screen.dart';
 import 'package:insta_ui_only/screens/MainPageScreen_Feeds/homeBar_screen.dart';
 import 'package:insta_ui_only/widgets/BottomNavBar/bottomNavBar_main.dart';
 import 'package:insta_ui_only/widgets/PostWidget/profilePhoto_widget.dart';
 import 'package:insta_ui_only/widgets/StoriesWidget/stories_widget.dart';
-import 'package:insta_ui_only/models/user.dart' as user;
+
 import 'package:insta_ui_only/widgets/followButton_widget.dart';
 import 'package:multiple_stream_builder/multiple_stream_builder.dart';
 import 'editProfile_screen.dart';
@@ -28,6 +26,7 @@ class AccountPage extends StatefulWidget {
 class _AccountPageState extends State<AccountPage> {
   // final user.User data = MyUserData().currentUser;
   final _db = FirebaseFirestore.instance;
+
   final _auth = FirebaseAuth.instance;
 
   @override
@@ -155,9 +154,8 @@ class _AccountPageState extends State<AccountPage> {
                                     Column(
                                       children: [
                                         Text(
-                                          "${snapshots.item1.data.docs.length}" ??
-                                              "",
-                                        ),
+                                            "${snapshots.item1.data.docs.length}" ??
+                                                ''),
                                         Text("Posts"),
                                       ],
                                     ),
@@ -198,7 +196,7 @@ class _AccountPageState extends State<AccountPage> {
                                 width: double.infinity,
                                 child: TextButton(
                                   onPressed: () {
-                                    db.FirebaseAuth.instance.signOut();
+                                    _auth.signOut();
                                     Navigator.of(context)
                                         .pushNamedAndRemoveUntil(
                                       LogIn.route,
@@ -257,52 +255,7 @@ class _AccountPageState extends State<AccountPage> {
                                     snapshots.item2.data['followers'] as List)
                                 : FollowButton("Follow", currentUser,
                                     snapshots.item2.data['followers'] as List),
-                        // : Padding(
-                        //     padding: const EdgeInsets.only(bottom: 8.0),
-                        //     child: Center(
-                        //       child: Container(
-                        //         width:
-                        //             MediaQuery.of(context).size.width * 0.5,
-                        //         child: GestureDetector(
-                        //           onTap: () {
-                        //             setState(
-                        //               () {
-                        //                 // widget.whichOne = 1.1;
-                        //               },
-                        //             );
-                        //           },
-                        //           child: Ink(
-                        //             decoration: BoxDecoration(
-                        //               color: Colors.blue,
-                        //               borderRadius:
-                        //                   BorderRadius.circular(5),
-                        //               border: Border.fromBorderSide(
-                        //                   BorderSide.none),
-                        //             ),
-                        //             child: InkWell(
-                        //               borderRadius:
-                        //                   BorderRadius.circular(10),
-                        //               child: Container(
-                        //                 height: 30,
-                        //                 width: 60,
-                        //                 child: Center(
-                        //                   child: Text(
-                        //                     'Follow',
-                        //                     style: TextStyle(
-                        //                       color: Colors.white,
-                        //                       fontWeight: FontWeight.w600,
-                        //                       fontSize: 17.0,
-                        //                     ),
-                        //                   ),
-                        //                 ),
-                        //               ),
-                        //             ),
-                        //           ),
-                        //         ),
-                        //       ),
-                        //     ),
-                        //   ),
-                        // MySpaces.vGapInBetween,
+                        MySpaces.vGapInBetween,
                         Padding(
                           padding: const EdgeInsets.only(bottom: 10),
                           child: Container(
@@ -359,108 +312,6 @@ class _AccountPageState extends State<AccountPage> {
                           ),
                         ),
                         MySpaces.hSmallGapInBetween,
-                        // (currentUser == FirebaseAuth.instance.currentUser.uid)
-                        //     ? Padding(
-                        //         padding: const EdgeInsets.only(bottom: 10),
-                        //         child: Container(
-                        //           width: double.infinity,
-                        //           child: TextButton(
-                        //             onPressed: () {
-                        //               db.FirebaseAuth.instance.signOut();
-                        //               Navigator.of(context).pushNamedAndRemoveUntil(
-                        //                 SignUp.route,
-                        //                 (route) => false,
-                        //               );
-                        //             },
-                        //             child: Row(
-                        //               mainAxisAlignment: MainAxisAlignment.center,
-                        //               children: [
-                        //                 Icon(
-                        //                   Icons.logout_outlined,
-                        //                   color: MediaQuery.of(context)
-                        //                               .platformBrightness ==
-                        //                           Brightness.light
-                        //                       ? Colors.black
-                        //                       : Colors.white,
-                        //                 ),
-                        //                 MySpaces.hGapInBetween,
-                        //                 Text(
-                        //                   "Log Out",
-                        //                   style: MyFonts.light
-                        //                       .setColor(MediaQuery.of(context)
-                        //                                   .platformBrightness ==
-                        //                               Brightness.dark
-                        //                           ? Colors.white
-                        //                           : Colors.black)
-                        //                       .size(17),
-                        //                 ),
-                        //               ],
-                        //             ),
-                        //             style: TextButton.styleFrom(
-                        //               backgroundColor: MediaQuery.of(context)
-                        //                           .platformBrightness ==
-                        //                       Brightness.light
-                        //                   ? Colors.white
-                        //                   : Colors.grey.shade900,
-                        //               shape: RoundedRectangleBorder(
-                        //                 side: BorderSide(
-                        //                   color: MediaQuery.of(context)
-                        //                               .platformBrightness ==
-                        //                           Brightness.dark
-                        //                       ? Colors.white.withOpacity(0.5)
-                        //                       : Colors.black.withOpacity(0.5),
-                        //                   width: 0.5,
-                        //                 ),
-                        //                 borderRadius: BorderRadius.all(
-                        //                   Radius.circular(8),
-                        //                 ),
-                        //               ),
-                        //             ),
-                        //           ),
-                        //         ),
-                        //       )
-                        //     : Center(
-                        //         child: Padding(
-                        //           padding: const EdgeInsets.only(bottom: 10),
-                        //           child: Container(
-                        //             width: MediaQuery.of(context).size.width * 0.5,
-                        //             child: GestureDetector(
-                        //               onTap: () {
-                        //                 setState(
-                        //                   () {
-                        //                     // widget.whichOne = 1.1;
-                        //                   },
-                        //                 );
-                        //               },
-                        //               child: Ink(
-                        //                 decoration: BoxDecoration(
-                        //                   color: Colors.blue,
-                        //                   borderRadius: BorderRadius.circular(5),
-                        //                   border: Border.fromBorderSide(
-                        //                       BorderSide.none),
-                        //                 ),
-                        //                 child: InkWell(
-                        //                   borderRadius: BorderRadius.circular(10),
-                        //                   child: Container(
-                        //                     height: 30,
-                        //                     width: 60,
-                        //                     child: Center(
-                        //                       child: Text(
-                        //                         'Follow',
-                        //                         style: TextStyle(
-                        //                           color: Colors.white,
-                        //                           fontWeight: FontWeight.w600,
-                        //                           fontSize: 17.0,
-                        //                         ),
-                        //                       ),
-                        //                     ),
-                        //                   ),
-                        //                 ),
-                        //               ),
-                        //             ),
-                        //           ),
-                        //         ),
-                        //       ),
                         MySpaces.vSmallestGapInBetween,
                         new Container(
                           child: new Column(
@@ -521,65 +372,6 @@ class _AccountPageState extends State<AccountPage> {
                       ],
                     ),
                   ),
-                  // GridView.builder(
-                  //   physics: NeverScrollableScrollPhysics(),
-                  //   shrinkWrap: true,
-                  //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  //     crossAxisCount: 3,
-                  //     crossAxisSpacing: 5,
-                  //     mainAxisSpacing: 5,
-                  //   ),
-                  //   itemCount: data.userPosts.length,
-                  //   itemBuilder: (ctx, index) {
-                  //     return Image.asset(
-                  //       data.userPosts[index],
-                  //       fit: BoxFit.cover,
-                  //     );
-                  //   },
-                  // )
-                  //         StreamBuilder(
-                  //           stream: _db
-                  //               .collection('posts')
-                  //               .where('addedBy',
-                  //                   isEqualTo: _db.doc(
-                  //                       '/users/${FirebaseAuth.instance.currentUser.uid}'))
-                  //               .snapshots(),
-                  //           builder: (ctx, AsyncSnapshot<QuerySnapshot> snapshot) {
-                  //             if (snapshot.hasError) {
-                  //               return Column(
-                  //                 children: [
-                  //                   SizedBox(height: 25),
-                  //                   Center(
-                  //                     child: Text(
-                  //                         "Something got wrong while fetching posts"),
-                  //                   ),
-                  //                 ],
-                  //               );
-                  //             }
-                  //             if (snapshot.connectionState == ConnectionState.waiting) {
-                  //               return Center(child: Text("Loading..."));
-                  //             }
-                  //             return GridView.builder(
-                  //               physics: NeverScrollableScrollPhysics(),
-                  //               shrinkWrap: true,
-                  //               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  //                 crossAxisCount: 3,
-                  //                 crossAxisSpacing: 5,
-                  //                 mainAxisSpacing: 5,
-                  //               ),
-                  //               itemCount: snapshot.data.docs.length,
-                  //               itemBuilder: (ctx, index) {
-                  //                 final data = snapshot.data.docs[index].data()
-                  //                     as Map<String, dynamic>;
-                  //                 return Image.network(data['imageUrl']);
-                  //               },
-                  //             );
-                  //           },
-                  //         ),
-                  //       ],
-                  //     ),
-                  //   ),
-                  // ),
                   ((snapshots.item2.data['followers'] as List)
                               .contains(_auth.currentUser.uid) ||
                           currentUser == _auth.currentUser.uid)
